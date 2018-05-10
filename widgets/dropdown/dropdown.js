46944a3f1537853d9159c2d49da542616b73fc79
ga35mav <ga35mav@mytum.de>
2018-05-10 15:06:48 +0200

    Created initial project with typescript

---
+++ widgets/dropdown/dropdown.js
@@ -0,0 +1,11 @@
+var Dropdown = /** @class */ (function () {
+    function Dropdown() {
+        this.elements = {};
+        this.init();
+    }
+    Dropdown.prototype.init = function () {
+        console.log("Init dropdown");
+    };
+    return Dropdown;
+}());
+new Dropdown();
---
+++ widgets/dropdown/dropdown.ts
@@ -0,0 +1,13 @@
+class Dropdown {
+    private elements = {};
+
+    constructor() {
+        this.init();
+    }
+
+    private init() {
+     console.log("Init dropdown");
+    }
+}
+
+new Dropdown();
