435e61822af8b9ee741023e26bdda2707d8f4f36
ga35mav <ga35mav@mytum.de>
2018-05-12 21:19:17 +0200

    Added tab focus and unfocus functionality

--- widgets/dropdown/dropdown.js
+++ widgets/dropdown/dropdown.js
@@ -13,6 +13,8 @@ var Dropdown = /** @class */ (function () {
         this.elements.options = this.widget.querySelector(".options");
         this.elements.optionArray = this.widget.querySelectorAll(".option");
         this.elements.currentOptionIndex = -1;
+        this.elements.tabElements = document.body.querySelectorAll("[tab-index]");
+        this.elements.currentFocused = -1;
         this.indexOptions();
         this.addEventHandlers();
         this.exposeApis();
@@ -28,36 +30,56 @@ var Dropdown = /** @class */ (function () {
     Dropdown.prototype.addEventHandlers = function () {
         this.keyOptions();
         this.mouseOptions();
+        this.tabFocusOptions();
+        this.addOpitonSelectedEvent();
+    };
+    Dropdown.prototype.addOpitonSelectedEvent = function () {
+        this.onSelectedEvent = new Event("onSelected");
+        this.elements.container.addEventListener("onSelected", function (e) {
+            console.log("Added custom dropdown event");
+        });
     };
     Dropdown.prototype.keyOptions = function () {
         var _this = this;
         document.body.addEventListener("keydown", function (e) {
+            if (!_this.elements.container.classList.contains("focused")) {
+                return;
+            }
             // logic for tab keys
             var keyCode = e.keyCode;
             switch (keyCode) {
                 case 9:
-                    _this.focusNext();
+                    // focus next tabable element
+                    // this.focusNext();
                     break;
                 case 37:
                 // left arrow key
                 case 38:
                     // up arrow
                     _this.highlightPreviousOption();
+                    e.preventDefault();
                     break;
                 case 39:
                 // right arrow
                 case 40:
                     // down arrow
                     _this.highlightNextOption();
+                    e.preventDefault();
                     break;
                 case 13:
                     // enter key
                     _this.setSelectedOption();
                     break;
+                case 32:
+                    // space key
+                    _this.toggleDropdown();
+                    e.preventDefault();
+                    break;
                 default:
                     // for shift tab key
                     if (e.shiftKey && keyCode === 9) {
-                        _this.focusPrevious();
+                        // focus previous tabable element
+                        // this.focusPrevious();
                     }
             }
         });
@@ -69,6 +91,26 @@ var Dropdown = /** @class */ (function () {
         });
         this.addClickHandlers();
         this.addHoverEffect();
+    };
+    Dropdown.prototype.tabFocusOptions = function () {
+        var _this = this;
+        // focus event handler
+        this.elements.container.addEventListener("focus", function (e) {
+            var target = e.target;
+            if (target === null) {
+                return;
+            }
+            target.classList.add("focused");
+        });
+        // focusout event handler
+        this.elements.container.addEventListener("focusout", function (e) {
+            var target = e.target;
+            if (target === null) {
+                return;
+            }
+            target.classList.remove("focused");
+            _this.setSelectedOption();
+        });
     };
     Dropdown.prototype.addClickHandlers = function () {
         var _this = this;
@@ -145,11 +187,30 @@ var Dropdown = /** @class */ (function () {
         if (option !== undefined) {
             this.elements.selectedElement.innerHTML = option.innerHTML;
             this.closeDropdown();
+            this.elements.container.dispatchEvent(this.onSelectedEvent);
         }
     };
     Dropdown.prototype.focusNext = function () {
+        this.elements.currentFocused++;
+        var tabable = this.elements.tabElements[this.elements.currentFocused];
+        if (tabable !== null && tabable !== undefined) {
+            this.clearFocused();
+            tabable.classList.add("focused");
+        }
+        if (this.elements.currentFocused >= this.elements.tabElements.length) {
+            this.currentFocused = this.elements.tabElements.length;
+        }
     };
     Dropdown.prototype.focusPrevious = function () {
+        this.elements.currentFocused--;
+        var tabable = this.elements.tabElements[this.elements.currentFocused];
+        if (tabable !== null && tabable !== undefined) {
+            this.clearFocused();
+            tabable.classList.add("focused");
+        }
+        if (this.elements.currentFocused < 0) {
+            this.currentFocused = -1;
+        }
     };
     Dropdown.prototype.indexOptions = function () {
         // add index number in each option
@@ -169,6 +230,15 @@ var Dropdown = /** @class */ (function () {
     Dropdown.prototype.closeDropdown = function () {
         this.elements.options.classList.remove("open");
     };
+    Dropdown.prototype.clearFocused = function () {
+        if (this.elements.tabElements === null || this.elements.tabElements.length <= 0) {
+            return;
+        }
+        for (var i = 0; i < this.elements.tabElements.length; i++) {
+            var tabable = this.elements.tabElements[i];
+            tabable.classList.remove("focused");
+        }
+    };
     return Dropdown;
 }());
 // init all dropdowns
@@ -176,4 +246,52 @@ var dropdowns = document.querySelectorAll(".selectDropdown");
 for (var i = 0; i < dropdowns.length; i++) {
     var dropdown = dropdowns[i];
     new Dropdown(dropdown);
+}
+var TestWidget = /** @class */ (function () {
+    function TestWidget(widget) {
+        this.widget = widget;
+        this.elements = {};
+        if (this.widget === null) {
+            return;
+        }
+        this.init();
+        this.submitOnOptionSelected();
+    }
+    TestWidget.prototype.init = function () {
+        this.elements.container = this.widget;
+        this.addEvents();
+    };
+    TestWidget.prototype.addEvents = function () {
+        // handle focus event
+        this.elements.container.addEventListener("focus", function (e) {
+            var target = e.target;
+            if (target === null) {
+                return;
+            }
+            target.classList.add("focused");
+        });
+        // handle unfocus event
+        this.elements.container.addEventListener("focusout", function (e) {
+            var target = e.target;
+            if (target === null) {
+                return;
+            }
+            target.classList.remove("focused");
+        });
+    };
+    TestWidget.prototype.submitOnOptionSelected = function () {
+        var dropdown = document.querySelector(".selectDropdown");
+        if (dropdown === null) {
+            return;
+        }
+        dropdown.addEventListener("onSelected", function (e) {
+            console.log("Submit form because an option is selected");
+        });
+    };
+    return TestWidget;
+}());
+var tabBoxes = document.querySelectorAll(".box");
+for (var i = 0; i < tabBoxes.length; i++) {
+    var box = tabBoxes[i];
+    new TestWidget(box);
 }
